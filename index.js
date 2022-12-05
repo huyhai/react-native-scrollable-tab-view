4c8c754db3a30208dd2af931281e3a821e5db5e1
huyhai <tranhuyhai8@gmail.com>
2022-12-05 13:08:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const React = require('react');
 const { Component } = React;
-const { ViewPropTypes } = ReactNative = require('react-native');
+ReactNative = require('react-native');
+
 const createReactClass = require('create-react-class');
 const PropTypes = require('prop-types');
 const {
@@ -41,12 +42,10 @@ const ScrollableTabView = createReactClass({
     onChangeTab: PropTypes.func,
     onScroll: PropTypes.func,
     renderTabBar: PropTypes.any,
-    tabBarUnderlineStyle: ViewPropTypes.style,
     tabBarBackgroundColor: PropTypes.string,
     tabBarActiveTextColor: PropTypes.string,
     tabBarInactiveTextColor: PropTypes.string,
     tabBarTextStyle: PropTypes.object,
-    style: ViewPropTypes.style,
     contentProps: PropTypes.object,
     scrollWithoutAnimation: PropTypes.bool,
     locked: PropTypes.bool,
